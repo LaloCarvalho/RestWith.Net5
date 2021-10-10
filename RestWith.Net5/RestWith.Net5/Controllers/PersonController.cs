cf08c782d78458dfcb223f94009f2d92d91b5e56
LaloCarvalho <eduardocdiogenes@gmail.com>
2021-10-09 22:14:34 -0300

    Teste Person

---
+++ RestWith.Net5/RestWith.Net5/Controllers/PersonController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RestWith.Net5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult GetSum(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid Inmput");
+        }
+    }
+}
